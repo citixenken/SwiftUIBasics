7a069ebe4c3792ce938d0eb5b21c11c536479a57
citixenken <kmuyesu@gmail.com>
2022-03-12 10:09:09 +0300

    data flow concepts

--- SwiftUIBasics/ContentView.swift
+++ SwiftUIBasics/ContentView.swift
@@ -8,22 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showLogo = true
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
+            if showLogo {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 250, height: 160)
+                    .padding()
+            }
+            
+            //Spacer()
+            
             Text("I love SwiftUI")
                 .font(.largeTitle)
                 .foregroundColor(.accentColor)
+            
+            //Spacer()
+            
             Text("SwiftUI makes developing iOS apps so easy and fun!")
             
-            Button(action: { print("Show logo...")}) {
-                Text("Click here to show logo!")
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color.blue)
-                    .cornerRadius(20)
-        }
-        .frame(width: 300, height: 100)
-        
+            //Spacer()
+
+            LogoButton(showLogo: $showLogo)
+            
         }
     }
 }
@@ -31,5 +41,23 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct LogoButton: View {
+    @Binding var showLogo: Bool
+    
+    var body: some View {
+        Button(action: {
+            showLogo.toggle()
+            
+        }) {
+            Text(showLogo ? "Click here to dismiss logo" : "Click here to show logo")
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(20)
+        }
+        .frame(width: 300, height: 100)
     }
 }
