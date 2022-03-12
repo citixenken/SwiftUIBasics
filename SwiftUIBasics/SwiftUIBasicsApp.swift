6cad34b2dc67931970c6a0c803be4949760ea52d
citixenken <kmuyesu@gmail.com>
2022-03-12 08:44:05 +0300

    first commit

---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Ken Muyesu on 12/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
